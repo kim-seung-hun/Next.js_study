e33eb9da7e9bbc115fffcf54579ccb23ccffd0c3
kim-seung-hun <rlatmdgns16@naver.com>
2022-11-13 17:50:03 +0900

    API라우트 연습

---
+++ 8.practice_3/nextjs-course-code-26a6b230c965996f99bd616a2e8617d295a8efad/components/input/comments.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
+
+function Comments(props) {
+  const { eventId } = props;
+
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+
+    useEffect(() => {
+      if (showComments) {
+        fetch("/api/comment" + eventId)
+          .then((res) => res.json())
+          .then((data) => {
+            setComments(data.comment);
+          });
+      }
+    }, [showComments]);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+    fetch("/api/comment" + eventId, {
+      method: "POST",
+      body: JSON.stringify(commentData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList items={comments} />}
+    </section>
+  );
+}
+
+export default Comments;
